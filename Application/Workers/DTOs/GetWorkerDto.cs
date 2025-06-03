61739da8b5627054e573b698db663cffb07b5983
Kamil Gniazdowski <110768094+Dredro@users.noreply.github.com>
2025-06-03 11:02:57 +0200

    Update GetWorkerDto.cs

--- Application/Workers/DTOs/GetWorkerDto.cs
+++ Application/Workers/DTOs/GetWorkerDto.cs
@@ -2,13 +2,13 @@ namespace Application.Workers.DTOs;
 
 public record GetWorkerDto
 (
-    string FirstName ,
-    string LastName ,
-    string Email ,
-    string PhoneNumber ,
-    decimal? Salary
+    string FirstName,
+    string LastName,
+    string Email,
+    string PhoneNumber,
+    decimal? Salary,
+    List<string> ServicesInProgress
 );
 ///TODO add 
 //  public ICollection<Notification> Notifications { get; set; } = [];
 //public ICollection<Notification> SentNotifications { get; set; } = [];
-//public ICollection<ServiceInProgress> ServiceInProgress { get; set; } = [];
