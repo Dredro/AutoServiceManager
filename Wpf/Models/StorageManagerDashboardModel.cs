319d143fbba00ca8412903cc9bd5bd44a0dad39c
= <mr.multik.zzz@gmail.com>
2025-06-07 23:32:05 +0200

    Added basic MVVM for StorageManagerDashboardPage.xaml

---
+++ Wpf/Models/StorageManagerDashboardModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Wpf.Models.DTOs;
+
+namespace Wpf.Models
+{
+    public class StorageManagerDashboardModel : INotifyPropertyChanged
+    {
+        private string _availablePartsSearchText = string.Empty;
+        private string _missingPartsSearchText = string.Empty;
+
+        
+        public ObservableCollection<SparePartsDTO> AvailableParts { get; set; } = new();
+        public ObservableCollection<SparePartsDTO> MissingParts { get; set; } = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ Wpf/ViewModel/StorageManagerDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Wpf.Models;
+
+namespace Wpf.ViewModel
+{
+    public class StorageManagerDashboardViewModel
+    {
+        public StorageManagerDashboardModel Model { get; set; } = new();
+
+        public StorageManagerDashboardViewModel()
+        {
+
+        }
+    }
+}
