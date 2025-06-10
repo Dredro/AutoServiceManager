6eb8b7333b5b57f2aa015e9dc8b539fb5fcce2e5
Jakub <jakub.jakacki18@gmail.com>
2025-06-10 09:11:19 +0200

    - Added commented sample pdf, - Fixes in PdfGenerator,

--- Wpf/Core/PdfGenerator.cs
+++ Wpf/Core/PdfGenerator.cs
@@ -16,6 +16,7 @@ namespace Wpf.Core
     {
         public static void GenerateInvoice(string filePath, OrderDTO order)
         {
+            QuestPDF.Settings.License = LicenseType.Community;
             if(order.IsPaid==false || order.FinalizationDate == null)
                 throw new InvalidOperationException("Nie można wygenerować faktury dla niezapłaconego zamówienia.");
             var document = QuestPDF.Fluent.Document.Create(container =>
@@ -25,105 +26,113 @@ namespace Wpf.Core
                     page.Margin(50);
                     page.Size(PageSizes.A4);
 
-                    page.Header().Text("FAKTURA VAT nr"+order.Id)
+                    page.Header().Text("FAKTURA VAT\nnr "+order.Id)
                         .SemiBold().FontSize(20).FontColor(Colors.Blue.Medium);
-
-                    page.Content().AlignLeft().Text($"Data ukończnia usługi: {order.FinalizationDate}")
+                    
+                    page.Content().Column(content => {
+                        content.Item().Height(30);
+                        content.Item().AlignLeft().Text($"Data ukończnia usługi: {order.FinalizationDate}")
                         .FontSize(12);
 
-                    page.Content().AlignLeft().Text($"Klient: {order.Client?.PersonalInfo?.FirstName} {order.Client?.PersonalInfo?.LastName}").FontSize(12);
-                    if(order.Vehicle != null)
-                    {
-                        page.Content().AlignLeft().Text($"Pojazd: {order.Vehicle.Make} {order.Vehicle.Model} ({order.Vehicle.RegistrationNumber})").FontSize(12);
-                    }
-                    page.Content().AlignCenter().Text($"Wykonane usługi").FontSize(16).FontColor(Colors.Blue.Medium);
-
-
-                    page.Content().Table(table =>
-                    {
-                        table.ColumnsDefinition(columns =>
+                        content.Item().AlignLeft().Text($"Klient: {order.Client?.PersonalInfo?.FirstName} {order.Client?.PersonalInfo?.LastName}").FontSize(12);
+                        if (order.Vehicle != null)
                         {
-                            columns.RelativeColumn(2); // Nazwa
-                            columns.RelativeColumn(5); // Opis
-                            columns.RelativeColumn(1); // Cena
-                        });
-
-                        // Header row
-                        table.Header(header =>
-                        {
-                            header.Cell().Element(CellStyle).Text("Usługa");
-                            header.Cell().Element(CellStyle).Text("Opis");
-                            header.Cell().Element(CellStyle).AlignRight().Text("Cena");
-
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).Background(Colors.Grey.Lighten3);
-                            }
-                        });
-
-                        // Data rows
-                        foreach (var servicesToDo in order.ServicesToDo)
-                        {
-                            table.Cell().Element(CellStyle).Text($"{servicesToDo.Service?.Name}");
-                            table.Cell().Element(CellStyle).Text($"{servicesToDo.Service?.Description}");
-                            table.Cell().Element(CellStyle).AlignRight().Text($"{servicesToDo.Price} zł");
-
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-                            }
+                            content.Item().AlignLeft().Text($"Pojazd: {order.Vehicle.Make} {order.Vehicle.Model} ({order.Vehicle.RegistrationNumber})").FontSize(12);
                         }
+                        content.Item().Height(50);
+                        content.Item().AlignCenter().Text($"Wykonane usługi").FontSize(16).FontColor(Colors.Blue.Medium);
+
+
+                        content.Item().Table(table =>
+                        {
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.RelativeColumn(2); // Nazwa
+                                columns.RelativeColumn(5); // Opis
+                                columns.RelativeColumn(1); // Cena
+                            });
+
+                            // Header row
+                            table.Header(header =>
+                            {
+                                header.Cell().Element(CellStyle).Text("Usługa");
+                                header.Cell().Element(CellStyle).Text("Opis");
+                                header.Cell().Element(CellStyle).AlignRight().Text("Cena");
+
+                                static IContainer CellStyle(IContainer container)
+                                {
+                                    return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).Background(Colors.Grey.Lighten3);
+                                }
+                            });
+
+                            // Data rows
+                            foreach (var servicesToDo in order.ServicesToDo)
+                            {
+                                table.Cell().Element(CellStyle).Text($"{servicesToDo.Service?.Name}");
+                                table.Cell().Element(CellStyle).Text($"{servicesToDo.Service?.Description}");
+                                table.Cell().Element(CellStyle).AlignRight().Text($"{servicesToDo.Price} zł");
+
+                                static IContainer CellStyle(IContainer container)
+                                {
+                                    return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+                                }
+                            }
+                        });
+                        content.Item().Height(10);
+                        content.Item().AlignCenter().Text($"Łączny koszt usług: {order.ServicesToDo.Sum(s => s.Price):F2} zł").FontSize(16).FontColor(Colors.Green.Darken1);
+                        content.Item().Height(50);
+                        content.Item().AlignCenter().Text($"Wykorzystane części zamienne").FontSize(16).FontColor(Colors.Blue.Medium);
+
+
+
+                        content.Item().Table(table =>
+                        {
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.RelativeColumn(2); // Numer katalogowy
+                                columns.RelativeColumn(3); // Nazwa
+                                columns.RelativeColumn(2); // Marka
+                                columns.RelativeColumn(1); // Jakość
+                                columns.RelativeColumn(1); // Ilość
+                                columns.RelativeColumn(1); // Cena
+                            });
+                            // Header row
+                            table.Header(header =>
+                            {
+                                header.Cell().Element(CellStyle).Text("Numer katalogowy");
+                                header.Cell().Element(CellStyle).Text("Nazwa");
+                                header.Cell().Element(CellStyle).Text("Marka");
+                                header.Cell().Element(CellStyle).Text("Jakość");
+                                header.Cell().Element(CellStyle).Text("Ilość");
+                                header.Cell().Element(CellStyle).AlignRight().Text("Cena");
+                                static IContainer CellStyle(IContainer container)
+                                {
+                                    return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).Background(Colors.Grey.Lighten3);
+                                }
+                            });
+                            // Data rows
+                            foreach (var sparePart in order.SpareParts)
+                            {
+                                table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.CatalogNumber}");
+                                table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Name}");
+                                table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Make}");
+                                table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Quality}");
+                                table.Cell().Element(CellStyle).Text($"{sparePart.Quantity}");
+                                table.Cell().Element(CellStyle).AlignRight().Text($"{sparePart.SparePart?.Price * sparePart.Quantity:F2} zł");
+                                static IContainer CellStyle(IContainer container)
+                                {
+                                    return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
+                                }
+                            }
+                        });
+                        content.Item().Height(10);
+                        content.Item().AlignCenter().Text($"Łączny koszt części zamiennych: {order.SpareParts.Sum(p => p.SparePart?.Price * p.Quantity):F2} zł").FontSize(16).FontColor(Colors.Green.Darken1);
+                        content.Item().Height(50);
+                        content.Item().AlignCenter().Text($"Łączny koszt zamówienia: {order.TotalCost:F2} zł").FontSize(18).FontColor(Colors.Green.Darken1);
+
                     });
 
-                    page.Content().AlignCenter().Text($"Łączny koszt usług: {order.ServicesToDo.Sum(s => s.Price):F2} zł").FontSize(16).FontColor(Colors.Green.Darken1);
-
-                    page.Content().AlignCenter().Text($"Wykorzystane części zamienne").FontSize(16).FontColor(Colors.Blue.Medium);
-
-
-
-                    page.Content().Table(table =>
-                    {
-                        table.ColumnsDefinition(columns =>
-                        {
-                            columns.RelativeColumn(2); // Numer katalogowy
-                            columns.RelativeColumn(3); // Nazwa
-                            columns.RelativeColumn(2); // Marka
-                            columns.RelativeColumn(1); // Jakość
-                            columns.RelativeColumn(1); // Ilość
-                            columns.RelativeColumn(1); // Cena
-                        });
-                        // Header row
-                        table.Header(header =>
-                        {
-                            header.Cell().Element(CellStyle).Text("Numer katalogowy");
-                            header.Cell().Element(CellStyle).Text("Nazwa");
-                            header.Cell().Element(CellStyle).Text("Marka");
-                            header.Cell().Element(CellStyle).Text("Jakość");
-                            header.Cell().Element(CellStyle).Text("Ilość");
-                            header.Cell().Element(CellStyle).AlignRight().Text("Cena");
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).Background(Colors.Grey.Lighten3);
-                            }
-                        });
-                        // Data rows
-                        foreach (var sparePart in order.SpareParts)
-                        {
-                            table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.CatalogNumber}");
-                            table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Name}");
-                            table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Make}");
-                            table.Cell().Element(CellStyle).Text($"{sparePart.SparePart?.Quality}");
-                            table.Cell().Element(CellStyle).Text($"{sparePart.Quantity}");
-                            table.Cell().Element(CellStyle).AlignRight().Text($"{sparePart.SparePart?.Price * sparePart.Quantity:F2} zł");
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-                            }
-                        }
-                    });
-
-                    page.Content().AlignCenter().Text($"Łączny koszt części zamiennych: {order.SpareParts.Sum(p => p.SparePart?.Price * p.Quantity):F2} zł").FontSize(16).FontColor(Colors.Green.Darken1);
-                    page.Content().AlignCenter().Text($"Łączny koszt zamówienia: {order.TotalCost:F2} zł").FontSize(18).FontColor(Colors.Green.Darken1);
+                    
                     page.Footer().AlignCenter().Text($"Wygenerowano: {DateTime.Now:dd.MM.yyyy HH:mm}");
                 });
             });
--- Wpf/MainWindow.xaml.cs
+++ Wpf/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Configuration;
 using System.Windows;
 using System.Windows.Controls;
+using Wpf.Core;
+using Wpf.Models.DTOs;
 using Wpf.ViewModel;
 using Wpf.Views;
 
@@ -11,6 +13,69 @@ namespace Wpf
         public MainWindow()
         {
             InitializeComponent();
+            //string filepath = "I:\\plik.pdf";
+            //OrderDTO order = new OrderDTO
+            //{
+            //    Id = Guid.NewGuid(),
+            //    FinalizationDate = DateOnly.FromDateTime(DateTime.Now),
+            //    IsPaid = true,
+            //    Client = new ClientDTO { 
+            //        Id=Guid.NewGuid(),
+            //        PersonalInfo = new PersonalInfo
+            //        {
+            //        FirstName = "Jan",
+            //        LastName = "Kowalski",
+            //    }, },
+            //    Vehicle = new VehicleDTO{Id=Guid.NewGuid(),
+            //        Make = "Toyota",
+            //        Model = "Corolla",
+            //        Vin = "1HGBH41JXMN109186",
+            //        RegistrationNumber = "ABC1234",
+            //        RegistrationDate = DateOnly.FromDateTime(DateTime.Now.AddYears(-2)),
+            //        YearOfProduction = 2021,
+            //        EngineCode = "2ZR-FE",
+            //        EngineDisplacement = 1800,
+            //        Power = 132.0m,
+            //        Type = VehicleType.Car,
+
+            //    },
+            //    ServicesToDo = new List<ServiceInProgressDTO>
+            //    {
+            //        new ServiceInProgressDTO
+            //        {
+            //            Id = Guid.NewGuid(),
+            //            StartDate = DateTime.Now.AddDays(-2),
+            //            EndDate = DateTime.Now.AddDays(-1),
+            //            Service = new ServiceDTO
+            //            {
+            //                Id = Guid.NewGuid(),
+            //                Name = "Wymiana oleju",
+            //                Description = "Wymiana oleju w silniku.",
+                           
+            //            },
+            //            ServiceStatus = ServiceStatus.Completed,
+            //            Price = 100.00m
+            //        },
+            //    },
+            //};
+
+            //order.SpareParts.Add(new OrderSparePartDTO
+            //{
+            //    SparePart = new SparePartsDTO
+            //    {
+            //        CatalogNumber = "MAX09",
+            //        Id = Guid.NewGuid(),
+            //        Name = "Filtr oleju",
+            //        Price = 20.00m,
+            //        Make = "Hilfo",
+            //        Quality = 'O',
+            //        QuantityInStock = 50,
+            //        Category = PartCategory.Powertrain,
+            //    },
+            //    Quantity = 1
+            //});
+
+            //PdfGenerator.GenerateInvoice(filepath,order);
         }
     }
 }
